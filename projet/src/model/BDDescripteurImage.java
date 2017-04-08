caa347ad42eb05fe1c17930806de55703d9e933c
Etienne <Etienne@192.168.1.2>
2017-04-08 22:07:32 +0200

    Mieux

--- projet/src/model/BDDescripteurImage.java
+++ projet/src/model/BDDescripteurImage.java
@@ -8,9 +8,10 @@ public class BDDescripteurImage {
 	//private List<DescripteurImage> listeDescripteurImage = new ArrayList<>();
 	private HashMap<String, DescripteurImage> listeDescripteurImage = new HashMap();
 	private BDDescripteurImage(){
-		listeDescripteurImage.put("imageRVB/testDomineRouge.jpg", new DescripteurImage("imageRVB/testDomineRouge.jpg", 47, 30, 23));
-		listeDescripteurImage.put("imageRVB/testDomineVert.jpg", new DescripteurImage("imageRVB/testDomineVert.jpg", 12, 56, 32));
-		listeDescripteurImage.put("imageRVB/testDomineBleu.jpg", new DescripteurImage("imageRVB/testDomineBleu.jpg", 17, 25, 58));
+		listeDescripteurImage.put("imageRVB/testDomineRouge1.jpg", new DescripteurImage("imageRVB/testDomineRouge1.jpg", 47, 30, 23));
+		listeDescripteurImage.put("imageRVB/testDomineRouge2.jpg", new DescripteurImage("imageRVB/testDomineRouge2.jpg", 35, 31, 34));
+		listeDescripteurImage.put("imageRVB/testDomineVert1.jpg", new DescripteurImage("imageRVB/testDomineVert1.jpg", 12, 56, 32));
+		listeDescripteurImage.put("imageRVB/testDomineVert2.jpg", new DescripteurImage("imageRVB/testDomineVert2.jpg", 17, 58, 25));
 	}
 	private static class BDDescripteurImageHolder{
 		private final static BDDescripteurImage instance = new BDDescripteurImage();
