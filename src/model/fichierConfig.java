7a09a4d05948ba3f31a942754d56432b267e41b7
Etienne Delarue <etienne.delarue31@gmail.com>
2017-04-22 12:14:04 +0200

    Trucs manquants

--- src/model/fichierConfig.java
+++ src/model/fichierConfig.java
@@ -12,13 +12,25 @@ public class fichierConfig {
 
 	private static fichierConfig instance;
 	private int seuilComparaisonFichier;
+	private int nbQuantif;
+	private int nbMots;
+	private String cheminBD;
 
 	/**
 	 * Dans le constructeur les attributs seront initialisé en fonction du
 	 * fichier de configuration.
 	 */
 	private fichierConfig() {
-		this.seuilComparaisonFichier = 50;
+		this.seuilComparaisonFichier = 80;
+		this.nbQuantif = 3;
+		this.nbMots = 5;
+	}
+	
+	public static fichierConfig getInstance() {
+		if (instance == null) {
+			instance = new fichierConfig();
+		}
+		return instance;
 	}
 
 	public int getSeuilComparaisonFichier() {
@@ -28,12 +40,29 @@ public class fichierConfig {
 	public void setSeuilComparaisonFichier(int seuilComparaisonFichier) {
 		this.seuilComparaisonFichier = seuilComparaisonFichier;
 	}
+	
+	public String getCheminBD(){
+		return this.cheminBD;
+	}
+	
+	public void setCheminBD(String c){
+		this.cheminBD = c;
+	}
 
-	public static fichierConfig getInstance() {
-		if (instance == null) {
-			instance = new fichierConfig();
-		}
-		return instance;
+	public int getNbQuantif() {
+		return nbQuantif;
+	}
+
+	public void setNbQuantif(int nbQuantif) {
+		this.nbQuantif = nbQuantif;
+	}
+
+	public int getNbMots() {
+		return nbMots;
+	}
+
+	public void setNbMots(int nbMots) {
+		this.nbMots = nbMots;
 	}
 
 }
