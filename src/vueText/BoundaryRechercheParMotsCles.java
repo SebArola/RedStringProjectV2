3fc239f82dbd2282e0b4aef7f66cf9a0bfb7f632
AurelienLG <a.leguillou@yahoo.fr>
2017-04-24 20:53:49 +0200

    Update BoundaryRechercheParMotsCles.java
    ajout hashmap

--- src/vueText/BoundaryRechercheParMotsCles.java
+++ src/vueText/BoundaryRechercheParMotsCles.java
@@ -1,6 +1,6 @@
 package vueText;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import controleur.ControlleurRechercheParMotsCles;
@@ -8,36 +8,36 @@ import controleur.ControlleurRechercheParMotsCles;
 public class BoundaryRechercheParMotsCles {
 
 	Scanner clavier = new Scanner(System.in);
-	private ControlleurRechercheParMotsCles crpmc;// = new ControlleurRechercheParMotsCles();
+	private ControlleurRechercheParMotsCles crpmc;// = new
+													// ControlleurRechercheParMotsCles();
 
 	public BoundaryRechercheParMotsCles(ControlleurRechercheParMotsCles controlrpmc) {
-		crpmc=controlrpmc;
+		crpmc = controlrpmc;
 	}
 
 	public void rechercheParMotsCles() {
-		ArrayList<String> listeTexte = new ArrayList<String>();
+		HashMap<Integer, String> mapTxt = new HashMap<Integer, String>();
 		boolean isCorrect;
 		String lecture;
 
 		do {
 			System.out.println("Veulliez rentrez les mots cles : ");
 			lecture = clavier.nextLine();
-			//System.out.println("LOL =" + lecture + "=");
+			// System.out.println("LOL =" + lecture + "=");
 			isCorrect = crpmc.verifierMotsCles(lecture);
 			if (!isCorrect)
 				System.out.println("Erreur de saisie Veuillez recommencer.");
 			else
 				System.out.println("Saisie Validée !");
 		} while (!isCorrect);
-		
-		
-		listeTexte=crpmc.rechercheTexte(lecture);
-		if (listeTexte.size() > 0)
-			for(int i=0;i< listeTexte.size();i++)
-				System.out.println("Resultat "+(i+1)+" = "+listeTexte.get(i));
+
+		mapTxt = crpmc.rechercheTexte(lecture);
+		if (mapTxt.size() > 0)
+			for (int i = 0; i < mapTxt.size(); i++)
+				System.out.println("Resultat " + (i + 1) + " = " + mapTxt.get(i));
 		else
 			System.out.println("La recherche n'as rien donné");
-			
+
 	}
 
 }
