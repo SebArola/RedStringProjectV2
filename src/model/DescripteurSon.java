aa5dc883321ec6699753e2d9e6a055b1f6ee67c1
AurelienLG <a.leguillou@yahoo.fr>
2017-04-25 13:37:51 +0200

    Update DescripteurSon.java
    aure modif

--- src/model/DescripteurSon.java
+++ src/model/DescripteurSon.java
@@ -1,46 +1,31 @@
 package model;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class DescripteurSon {
 	public String descripteur;
 	public String nom;
-	public int nombreOccurence;
-	
-	
-	public DescripteurSon(DescripteurSon ds){
-		this.descripteur =ds.descripteur ;
-		this.nom=ds.nom;
-		this.nombreOccurence=ds.nombreOccurence;
+	public int nbOccurence;
+
+	public DescripteurSon(DescripteurSon ds) {
+		this.descripteur = ds.descripteur;
+		this.nom = ds.nom;
+		this.nbOccurence = ds.nbOccurence;
 	}
-	
-	public DescripteurSon(String nom){
-		String lecture="";
-		try{
-			//InputStream ips=new FileInputStream(nom); 
-			//InputStreamReader ipsr=new InputStreamReader(ips);
-			BufferedReader br=new BufferedReader(new FileReader(nom));
-			String ligne="";
-			do{
-				ligne=br.readLine();
-				if(ligne!=null)
-					lecture+=ligne+"\n";
-			}while (ligne!=null);
-				
-			
-			br.close(); 
-		}		
-		catch (Exception e){
-			System.out.println(e.toString());
-		}
-		System.out.println("lecture: "+lecture);
-		this.descripteur=lecture;
-		System.out.println("lecture: "+descripteur);
-	    this.nom=nom;
-	    this.nombreOccurence=0;
+
+	public DescripteurSon(String lecture) {
+		String[] decoupe = lecture.split(";");
+		String[] decoupe2 = decoupe[0].split("<descripteur>");
+		this.nom = decoupe2[1];
+		this.descripteur = decoupe[1];
+		this.nbOccurence = 0;
 	}
-	public String toString(){
-		return("Nom :"+this.nom +" Descripteur :"+this.descripteur);
+
+	public String toString() {
+		return ("Nom :" + this.nom + " Descripteur :" + this.descripteur);
+	}
+	public String getDescripteur(){
+		return descripteur;
+	}
+	public String getNom(){
+		return nom;
 	}
 }
