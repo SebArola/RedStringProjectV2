862fabe138412e25e6b84a29eb683529f29f7f5e
Etienne Delarue <etienne.delarue31@gmail.com>
2017-04-25 22:02:22 +0200

    ss

--- src/test/TestComparaisonImage.java
+++ src/test/TestComparaisonImage.java
@@ -1,12 +1,14 @@
 package test;
 
+import java.io.IOException;
+
 import model.BDDescripteurImage;
 import vueText.VueComparaisonFichier;
 
 public class TestComparaisonImage {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BDDescripteurImage.getInstance().genBaseDescripteurImage();
 		VueComparaisonFichier vue = new VueComparaisonFichier();
 		vue.comparaisonFichier();
