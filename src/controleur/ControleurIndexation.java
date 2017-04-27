35fd63bb325df7610edfa22d38c6ca59bd70f313
RogersNtr <r.nengwe@yahoo.fr>
2017-04-27 04:19:37 +0200

    indexation ihm

--- src/controleur/ControleurIndexation.java
+++ src/controleur/ControleurIndexation.java
@@ -14,22 +14,23 @@ public class ControleurIndexation {
 		this.descripteur.addObserver(vueObservateurs);
 		
 	}
-	public String getDescripteur(TypeFichier type, int nbMots) {		
-		threadLancementIndexation=new ThreadLancementIndexation(type, nbMots);
-		Thread threadLancementIndexation1=new Thread(threadLancementIndexation);
-		threadLancementIndexation1.start();//On lance l'execution du code. grace a la methode waitfor() dans le thread, on va attendre jusqu'a
+	public String getDescripteur(TypeFichier type, int nbMots) {	
+		//System.out.println("je suis passe dans descripteur getDescripteur de ctrlindex");
+		//threadLancementIndexation=new ThreadLancementIndexation(type, nbMots);
+		//Thread threadLancementIndexation1=new Thread(threadLancementIndexation);
+		//threadLancementIndexation1.start();//On lance l'execution du code. grace a la methode waitfor() dans le thread, on va attendre jusqu'a
 		//ce que l'executio soit finit avant d'invoquer la fonction  getDescripteurGenere.		
 		//threadLancementIndexation.genererDescripteur(type, nbMots);
-		
-		try {
+		String descripteur1=descripteur.getDescripteurGenere(type, nbMots);
+		//try {
 		//Thread.sleep(4000);
-			this.wait(40000);
-		} catch (Exception e) {
+		//	this.wait(40000);
+		//} catch (Exception e) {
 		// TODO: handle exception
-		}
+		//}
 		/*String descripteur1=*///descripteur.getDescripteurGenere(type, nbMots);	
-		String descripteur1=threadLancementIndexation.getDescripteurGenere();
-		System.out.println(descripteur1);
+		//String descripteur1=threadLancementIndexation.getDescripteurGenere();
+		//System.out.println(descripteur1);
 		return descripteur1;
 	}
 
